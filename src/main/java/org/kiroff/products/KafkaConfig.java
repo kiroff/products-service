5e9b6fc6845ceb65b79ea7784b0a9e1670c255d6
Boyan G. Kiroff <boyan.kiroff@brandmaker.com>
2026-03-24 15:01:44 +0200

    Added idempotent producer

--- src/main/java/org/kiroff/products/KafkaConfig.java
+++ src/main/java/org/kiroff/products/KafkaConfig.java
@@ -1,15 +1,70 @@
 package org.kiroff.products;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.kiroff.products.events.ProductCreatedEvent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class KafkaConfig
 {
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
+    @Value("${spring.kafka.producer.key-serializer}")
+    private String keySerializerClassConfig;
+    @Value("${spring.kafka.producer.value-serializer}")
+    private String valueSerializerClassConfig;
+    @Value("${spring.kafka.producer.acks}")
+    private String acksConfig;
+    @Value("${spring.kafka.producer.properties.delivery.timeout.ms}")
+    private String deliveryTimeoutMsConfig;
+    @Value("${spring.kafka.producer.properties.linger.ms}")
+    private String lingerMsConfig;
+    @Value("${spring.kafka.producer.properties.request.timeout.ms}")
+    private String requestTimeoutMsConfig;
+    @Value("${spring.kafka.producer.properties.enable.idempotence}")
+    private String enableIdempotenceConfig;
+    @Value("${spring.kafka.producer.properties.max.in.flight.requests.per.connection}")
+    private String maxInFlightRequestsPerConnection;
+
+    @Bean
+    Map<String, Object> producerConfigs()
+    {
+        final Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializerClassConfig);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializerClassConfig);
+        props.put(ProducerConfig.ACKS_CONFIG, acksConfig);
+        props.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeoutMsConfig);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, lingerMsConfig);
+        props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeoutMsConfig);
+        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, enableIdempotenceConfig);
+        props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, maxInFlightRequestsPerConnection);
+//        props.put(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);//Default
+        return props;
+    }
+
+    @Bean
+    ProducerFactory<String, ProductCreatedEvent> producerFactory()
+    {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate()
+    {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
     @Bean
     NewTopic createTopic() {
         return TopicBuilder.name("product-created-events-topic")
--- src/main/java/org/kiroff/products/services/ProductServiceImpl.java
+++ src/main/java/org/kiroff/products/services/ProductServiceImpl.java
@@ -40,7 +40,7 @@ public class ProductServiceImpl implements ProductService
 ////                .join();//to block/syncronze the client/request
 //        LOGGER.info("Created product with id {}", productId);
         final SendResult<String, ProductCreatedEvent> result = kafkaTemplate.send(
-                "product-created-events-topic", productId, event).get();
+                "batcata", productId, event).get();
         LOGGER.info("Sent to topic= {}, partition={}, offset={}", result.getRecordMetadata().topic(), result.getRecordMetadata().partition(), result.getRecordMetadata().offset());
         return productId;
     }
